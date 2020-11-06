52149923eaea2f2f531f0e14fcbb7b77b3d2b182
dan-du-car <dan.du@leidos.com>
2020-11-06 10:07:09 -0800

    set defaults speed  limit

--- website/widgets/cruising/widget.js
+++ website/widgets/cruising/widget.js
@@ -212,7 +212,11 @@ CarmaJS.WidgetFramework.Cruising = (function () {
                 setSpeedometer(actualSpeedMPH);
             });
         };
-
+        var setDefaultSpeedLimit = function (){
+            if (document.getElementById('divSpeedLimitValue') == null)
+                    return;
+            document.getElementById('divSpeedLimitValue').innerHTML = CarmaJS.Config.getSPEEDLIMIT();
+        }
         /***
         * Custom widgets using JQuery Widget Framework.
         * NOTE: that widget framework namespace can only be one level deep.
@@ -241,12 +245,15 @@ CarmaJS.WidgetFramework.Cruising = (function () {
                 this.element.empty();
                 this._super();
              },
+             setDefaultSpeedLimit: function(){
+                setDefaultSpeedLimit();
+             },
              checkRouteState: function(){
                 checkRouteState();
              },
              showActiveRoute: function(){
                 showActiveRoute();
-             }
+             },
         });//CarmaJS.cruisingSpeedLimit
 
         $.widget("CarmaJS.cruisingSpeedCmd", {
@@ -305,6 +312,7 @@ CarmaJS.WidgetFramework.Cruising = (function () {
         var loadCustomWidget = function(container) {
 
             container.cruisingSpeedLimit();
+            container.cruisingSpeedLimit("setDefaultSpeedLimit",null);
             container.cruisingSpeedLimit("showActiveRoute",null);
             container.cruisingSpeedLimit("checkRouteState",null);
 

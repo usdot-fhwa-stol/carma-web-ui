2edac3f9897cfdf115965d29227f67c60c4cb38e
dan-du-car <dan.du@leidos.com>
2021-07-09 15:52:27 -0400

    init

--- website/utils/platooning_info/platooning_info.js
+++ website/utils/platooning_info/platooning_info.js
@@ -27,7 +27,7 @@ function createPlatooningInfo(platooning_state,host_vehicle_position,desired_gap
     p_host_div.appendChild(p_host_desired_gap);
 
     let p_host_actual_gap = document.createElement('p');
-    p_host_actual_gap.innerHTML = "ACTUAL GAP FROM LEADER: " + actual_gap;
+    p_host_actual_gap.innerHTML = "ACTUAL GAP FROM LEADER: " + actual_gap + " METERS";
     p_host_actual_gap.id = 'platooning_actual_gap_id';
     p_host_div.appendChild(p_host_actual_gap);
 
@@ -50,7 +50,7 @@ function createPlatooningInfo(platooning_state,host_vehicle_position,desired_gap
     p_leader_div.appendChild(p_platoon_id);
 
     let p_platoon_applied_speed = document.createElement('p');
-    p_platoon_applied_speed.innerHTML = "PLATOON APPLIED SPEED: " + platoon_applied_speed;
+    p_platoon_applied_speed.innerHTML = "PLATOON APPLIED SPEED: " + platoon_applied_speed+ " MPH";
     p_platoon_applied_speed.id = 'platooning_applied_speed_id';
     p_leader_div.appendChild(p_platoon_applied_speed);
 
@@ -61,10 +61,15 @@ function createPlatooningInfo(platooning_state,host_vehicle_position,desired_gap
 
 function updatePlatooningInfo(platooning_state,host_vehicle_position,desired_gap,actual_gap,leader_vehicle_id,platoon_id,platoon_applied_speed)
 {
+    if(actual_gap < 0)
+        actual_gap ='N/A';
+    
+    if(desired_gap < 0)
+        desired_gap ='N/A';
    document.getElementById('platooning_host_state_id').innerHTML ="PLATOON STATE: " + platooning_state;
    document.getElementById('platooning_host_position_id').innerHTML = "POSITION: " + host_vehicle_position;
    document.getElementById('platooning_desired_gap_id').innerHTML ="DESIRED GAP FROM LEADER: " + desired_gap + " METERS";
-   document.getElementById('platooning_actual_gap_id').innerHTML = "ACTUAL GAP FROM LEADER: " + actual_gap;
+   document.getElementById('platooning_actual_gap_id').innerHTML = "ACTUAL GAP FROM LEADER: " + actual_gap + " METERS";
    document.getElementById('platooning_leader_vehicle_id').innerHTML ="VEHICLE ID: " +  leader_vehicle_id;
    document.getElementById('platooning_applied_speed_id').innerHTML ="PLATOON APPLIED SPEED: " +  platoon_applied_speed + " MPH";
    document.getElementById('platooning_id').innerHTML =  "PLATOON ID: " + platoon_id;

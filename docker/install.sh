0bcfe0f726862c4c11c06c7976b00460fc5424e4
Adam Morrissett <adam.morrissett.ctr@dot.gov>
2021-07-14 08:58:37 -0400

    Modify Docker files to support arm64 platform
    Currently, the CARMA Web UI Docker image only supports building for x86
    targets. This commit changes support for building arm64 targets.
    
    Dockerfile
    * change PHP base image to use the arm64 version
    
    docker/install.sh
    * change Docker download script to use the arm64 architecture

--- docker/install.sh
+++ docker/install.sh
@@ -25,7 +25,7 @@ apt-get -y install apt-transport-https \
         software-properties-common
 curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg > /tmp/dkey; apt-key add /tmp/dkey
 add-apt-repository \
-        "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+        "deb [arch=arm64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
         $(lsb_release -cs) \
         stable"
 
